7cb0d25e8521685418dba70b3888bf8d7757e036
Asimamir25 <asimamir32@gmail.com>
2024-10-09 02:52:13 +0500

    optimize image

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,37 @@
-import React from "react";
-import Infobar from "./components/infobar/Infobar";
-import Hero from "./components/hero/Hero";
-import Navbar from "./components/navbar/Navbar";
-import WhyChoose from "./components/whyChoose/WhyChoose";
-import DreamSmile from "./components/dreamSmile/DreamSmile";
-import ReviewSection from "./components/reviewSection/ReviewSection";
-import Finance from "./components/finance/Finance";
-import Gallery from "./components/gallery/Gallery";
-import PictureLayout from "./components/pictureLayout/PictureLayout";
-import Contact from "./components/contact/Contact";
-import Footer from "./components/footer/Footer";
-
-// index.js or App.js
+import React, { Suspense, lazy } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Policy from "./components/footer/Policy";
+
+// Lazy load your components
+const Infobar = lazy(() => import("./components/infobar/Infobar"));
+const Hero = lazy(() => import("./components/hero/Hero"));
+const Navbar = lazy(() => import("./components/navbar/Navbar"));
+const WhyChoose = lazy(() => import("./components/whyChoose/WhyChoose"));
+const DreamSmile = lazy(() => import("./components/dreamSmile/DreamSmile"));
+const ReviewSection = lazy(() => import("./components/reviewSection/ReviewSection"));
+const Finance = lazy(() => import("./components/finance/Finance"));
+const Gallery = lazy(() => import("./components/gallery/Gallery"));
+const PictureLayout = lazy(() => import("./components/pictureLayout/PictureLayout"));
+const Contact = lazy(() => import("./components/contact/Contact"));
+const Footer = lazy(() => import("./components/footer/Footer"));
+const Policy = lazy(() => import("./components/footer/Policy"));
 
 const App = () => {
   return (
     <div>
-      <Infobar />
-      <Navbar />
-      <Hero />
-      <PictureLayout />
-      <WhyChoose />
-      <DreamSmile />
-      <ReviewSection />
-      <Finance />
-      <Gallery />
-      <Contact />
-      <Footer />
-      <Policy/>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Infobar />
+        <Navbar />
+        <Hero />
+        <PictureLayout />
+        <WhyChoose />
+        <DreamSmile />
+        <ReviewSection />
+        <Finance />
+        <Gallery />
+        <Contact />
+        <Footer />
+        <Policy />
+      </Suspense>
     </div>
   );
 };
