3d39fdfe23ea85375b003ed3efb780fb73e6d5b6
asimamir <asimamir32@gmail.com>
2024-10-22 22:15:53 +0500

    fixes

--- src/components/pictureLayout/PictureLayout.jsx
+++ src/components/pictureLayout/PictureLayout.jsx
@@ -11,7 +11,7 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
 
-const ImageScroll = () => {
+const PictureLayout = () => {
   const imagesLeftColumn = [
     Lady,
     Lady2,
@@ -85,46 +85,46 @@ const ImageScroll = () => {
   }, []);
 
   return (
-    <div className=" container grid grid-cols-1 md:grid-cols-3  h-full w-screen overflow-hidden">
+    <div className="container mx-auto grid grid-cols-1 md:grid-cols-3 h-full w-full overflow-hidden">
       {/* Left Column (Images) */}
-      <div className="relative flex items-center justify-center ">
-        <div className="flex flex-col gap-[12px] absolute animate-scroll">
+      <div className="relative flex items-center justify-center">
+        <div className="flex flex-col gap-2 absolute animate-scroll">
           {imagesLeftColumn.map((image, index) => (
             <img
               key={index}
               src={image}
               alt={`Image ${index + 1}`}
-              className="w-[100%] h-auto hidden md:block"
+              className="w-full h-auto hidden md:block"
             />
           ))}
         </div>
       </div>
 
       {/* Center Content (Text) */}
-      <div className="w-full flex flex-col text-center items-center   mb-4 md:mb-0">
+      <div className="w-full flex flex-col text-center items-center mb-4 md:mb-0">
         <div className="mt-3">
           <h2
             ref={(el) => (titleRefs.current[0] = el)}
-            className="text-[32px] md:text-[56px] font-bold font-openSans"
+            className="text-32 md:text-56 font-bold font-openSans"
           >
             Your smile,
           </h2>
           <h2
             ref={(el) => (titleRefs.current[1] = el)}
-            className="text-[32px] md:text-[56px] font-bold font-openSans"
+            className="text-32 md:text-56 font-bold font-openSans"
           >
             Our passion
           </h2>
         </div>
         <p
           ref={(el) => (paragraphRefs.current[0] = el)}
-          className="mt-2 text-[24px] text-[#9E7F618A] px-[14px] font-bold italic font-openSans"
+          className="mt-2 text-24 text-[#9E7F618A] px-4 font-bold italic font-openSans"
         >
           Quality dental care in the heart of Cheltenham
         </p>
         <p
           ref={(el) => (paragraphRefs.current[1] = el)}
-          className="text-center text-[14px] lg:[24px] 2xl:text-[40px] px-3 mt-[34px] font-openSans"
+          className="text-center text-14 lg:text-24 2xl:text-40 px-3 mt-8 font-openSans"
         >
           At NuYu Dental, we believe in the power of a healthy smile. Our
           experienced team offers personalised care using the latest technology
@@ -135,27 +135,27 @@ const ImageScroll = () => {
         </p>
         <h4
           ref={(el) => (titleRefs.current[2] = el)}
-          className="text-[34px] font-normal mt-[32px] font-openSans"
+          className="text-34 font-normal mt-8 font-openSans"
         >
           New Smile, NuYu
         </h4>
-        <div className="flex items-center mt-[27px]">
+        <div className="flex items-center mt-7">
           <div className="flex px-6 py-3 rounded-md items-center justify-center gap-2 border border-[#9E7F61] group hover:bg-[#dac0a7] transition-colors ease-in">
-            <button className="text-black  group transition-all duration-200">
+            <button className="text-black group transition-all duration-200">
               <span className="group-hover:text-white font-openSans">Meet the Team</span>
             </button>
             <img
-    src={Arrow}
-    alt="arrow"
-    className="h-4 mt-1 transition-all duration-200 cursor-pointer group-hover:fill-current group-hover:hidden"
-  />
+              src={Arrow}
+              alt="arrow"
+              className="h-4 mt-1 transition-all duration-200 cursor-pointer group-hover:fill-current group-hover:hidden"
+            />
           </div>
         </div>
       </div>
 
       {/* Right Column (Images) */}
       <div className="relative flex items-center justify-center">
-        <div className="flex flex-col gap-[12px] absolute animate-scroll-reverse ">
+        <div className="flex flex-col gap-2 absolute animate-scroll-reverse">
           {imagesRightColumn.map((image, index) => (
             <img
               key={index}
@@ -170,4 +170,4 @@ const ImageScroll = () => {
   );
 };
 
-export default ImageScroll;
+export default PictureLayout;
